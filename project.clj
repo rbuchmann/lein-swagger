26f66a32e31b7a0ccbd53b89fdb609a1ca97ea91
Rasmus Buchmann <rasmus.buchmann@gmx.de>
2016-09-04 22:31:13 +0200

    Start simpler http client generation

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "http://github.com/rbuchmann/lein-swagger"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[io.swagger/swagger-codegen "2.2.1"]]
+  :dependencies [[io.swagger/swagger-codegen "2.2.1"]
+                 [org.clojure/data.json "0.2.6"]
+                 [camel-snake-kebab "0.4.0"]]
   :eval-in-leiningen true)
--- src/leiningen/swagger.clj
+++ src/leiningen/swagger.clj
@@ -4,8 +4,12 @@
                                CodegenConfig
                                DefaultGenerator)
            io.swagger.codegen.config.CodegenConfigurator)
-  (:require [leiningen.core.main :as lein]
-            [clojure.edn         :as edn]))
+  (:require [leiningen.core.main    :as lein]
+            [clojure.edn            :as edn]
+            [clojure.data.json      :as json]
+            [camel-snake-kebab.core :refer [->kebab-case-symbol
+                                            ->kebab-case-keyword]]
+            [clojure.pprint         :refer [code-dispatch with-pprint-dispatch pprint]]))
 
 (defn check-keys [m ks]
   (when-let [k (some #(when-not (m %)
@@ -35,6 +39,62 @@
        (partition 2)
        (map (fn [[k v]] [(edn/read-string k) v]))
        (into {})))
+
+(defn kfn [s]
+  (if (.startsWith s "/")
+    s
+    (keyword s)))
+
+(def tj (json/read-str (slurp "evaluate.article.json") :key-fn kfn))
+
+(def tp (first (:paths tj)))
+
+(defn method->symbol [method]
+  (symbol "http" (name method)))
+
+(def symbolize-name (comp ->kebab-case-symbol :name))
+(def keywordize-name (comp ->kebab-case-keyword :name))
+
+(defn param-vector [parameters]
+  (let [{:keys [required optional]} (group-by #(if (:required %)
+                                                 :required
+                                                 :optional)
+                                              parameters)
+        optionals-map {:keys (mapv symbolize-name optional)}]
+    (-> (mapv symbolize-name required)
+        (conj '& optionals-map))))
+
+(defmulti gen-param-type first)
+
+(defmethod gen-param-type "query" [[_ params]]
+  {:query-params
+   (into {} (map (juxt :name symbolize-name) params))})
+
+(defmethod gen-param-type "header" [[_ params]]
+  {:headers
+   (into {} (map (juxt :name symbolize-name) params))})
+
+(defn gen-parameters [parameters]
+  (let [params-by-type (group-by :in parameters)]
+    (map gen-param-type params-by-type)))
+
+(defn gen-method [path method info]
+  (let [{:keys [parameters summary operationId produces]} info
+        method-name (->kebab-case-symbol operationId)]
+    (list 'defn method-name summary (param-vector parameters)
+       ((method->symbol method) path
+        (apply merge (gen-parameters parameters))))))
+
+(defn gen-path [path desc]
+  (for [[method info] desc]
+    (gen-method path method info)))
+
+(defn format-method [form]
+  (with-pprint-dispatch code-dispatch (pprint form)))
+
+(defn gen-custom [src]
+  (doseq [s (map format-method (apply gen-path src))]
+    (println s)))
 
 (defn swagger
   "Generates a swagger client from the specifications. Mandatory arguments:\n:name -> client project name\n:input -> swagger input file location\n:path -> output path"
