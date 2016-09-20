cfa7fb0982e45c8c23f05d34dbaaf90667d0bfb6
Rasmus Buchmann <rasmus.buchmann@gmx.de>
2016-09-20 23:30:04 +0200

    Beautiful, simple clj-http based clients

--- src/leiningen/swagger.clj
+++ src/leiningen/swagger.clj
@@ -9,30 +9,15 @@
             [clojure.data.json      :as json]
             [camel-snake-kebab.core :refer [->kebab-case-symbol
                                             ->kebab-case-keyword]]
-            [clojure.pprint         :refer [code-dispatch with-pprint-dispatch pprint]]))
+            [clojure.pprint         :refer [code-dispatch with-pprint-dispatch pprint]]
+            [clojure.string         :as str]
+            [clojure.java.io        :as io]))
 
 (defn check-keys [m ks]
   (when-let [k (some #(when-not (m %)
                         %)
                      ks)]
     (lein/abort "Missing required key:" k)))
-
-(defn gen-swagger [opts]
-  (check-keys opts [:input :path :name])
-  (let [{:keys [input path name]} opts
-        config-input (.toClientOptInput (doto (CodegenConfigurator.)
-                                          (.setLang "clojure")
-                                          (.setInputSpec input)
-                                          (.setOutputDir path)))
-        additional-options (doto (-> config-input
-                                     (.getConfig)
-                                     (.additionalProperties))
-                             (.put "projectName" name))
-        gen (-> (DefaultGenerator.)
-                (.opts config-input))]
-    (try (.generate gen)
-         (catch Exception e
-           (lein/abort "An error occurred: " e)))))
 
 (defn parse-args [args]
   (->> args
@@ -44,10 +29,6 @@
   (if (.startsWith s "/")
     s
     (keyword s)))
-
-(def tj (json/read-str (slurp "evaluate.article.json") :key-fn kfn))
-
-(def tp (first (:paths tj)))
 
 (defn method->symbol [method]
   (symbol "http" (name method)))
@@ -74,29 +55,68 @@
   {:headers
    (into {} (map (juxt :name symbolize-name) params))})
 
+(defn patternize-name [param]
+  (re-pattern (str "\\{" (:name param) "\\}")))
+
+(defmethod gen-param-type "path" [[_ params]]
+  (when (not-empty params)
+    {:path-params (map (juxt patternize-name symbolize-name) params)}))
+
 (defn gen-parameters [parameters]
   (let [params-by-type (group-by :in parameters)]
     (map gen-param-type params-by-type)))
 
+(defn gen-param-path [path params]
+  (when (not-empty params)
+    `(~'-> ~path
+      ~@(for [p params]
+          (list* 'str/replace p)))))
+
 (defn gen-method [path method info]
   (let [{:keys [parameters summary operationId produces]} info
-        method-name (->kebab-case-symbol operationId)]
+        method-name (->kebab-case-symbol operationId)
+        client-params (apply merge (gen-parameters parameters))
+        url (or (gen-param-path path (:path-params client-params)) path)]
     (list 'defn method-name summary (param-vector parameters)
-       ((method->symbol method) path
-        (apply merge (gen-parameters parameters))))))
+       (list (method->symbol method) url
+        (dissoc client-params :path-params)))))
 
 (defn gen-path [path desc]
   (for [[method info] desc]
     (gen-method path method info)))
 
 (defn format-method [form]
-  (with-pprint-dispatch code-dispatch (pprint form)))
+  (with-out-str
+    (with-pprint-dispatch code-dispatch (pprint form))))
 
-(defn gen-custom [src]
-  (doseq [s (map format-method (apply gen-path src))]
-    (println s)))
+(defn gen-header [name]
+  (format-method
+   (list 'ns (symbol (str "swagger-clients." name))
+         '(:require [clj-http.client :as http]))))
 
+(defn filenamify [s]
+  (str "src/swagger-clients/"
+       (str/replace s #"-" "_")
+       ".clj"))
+
+(defn gen-source [src]
+  (str/join "\n" (->> src
+                      :paths
+                      (map #(apply gen-path %))
+                      (apply concat)
+                      (map format-method))))
+
+(defn gen-swagger [src name]
+  (let [json (json/read-str (slurp src) :key-fn kfn)
+        source (gen-source json)
+        target (filenamify name)]
+    (io/make-parents target)
+    (spit target (str (gen-header name)
+               "\n"
+               source))))
 (defn swagger
   "Generates a swagger client from the specifications. Mandatory arguments:\n:name -> client project name\n:input -> swagger input file location\n:path -> output path"
   [project & args]
-  (gen-swagger (parse-args args)))
+  (let [{:keys [src name] :as m} (parse-args args)]
+    (check-keys m [:src :name])
+    (gen-swagger src name)))
